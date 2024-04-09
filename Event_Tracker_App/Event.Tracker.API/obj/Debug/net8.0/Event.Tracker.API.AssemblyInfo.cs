853a8994f87216f195fd9e5eb301d822afb1d6ed
william00771 <william.f.lindberg@hotmail.com>
2024-04-09 11:50:25 +0200

    setup: solution file, gitignore

--- Event_Tracker_App/Event.Tracker.API/obj/Debug/net8.0/Event.Tracker.API.AssemblyInfo.cs
+++ Event_Tracker_App/Event.Tracker.API/obj/Debug/net8.0/Event.Tracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8df82ecdf674d6d55d238f15b9aa06e1f06b037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c181a284939281ef128034b70c8c91e55ff3be9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
