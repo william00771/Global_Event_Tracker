7260717b9b47c90939bcde17dcbddce06a015030
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-10 19:43:16 +0200

    setup: leaflet

--- Event_Tracker_App/Event.Tracker.Web/src/pages/Explore.tsx
+++ Event_Tracker_App/Event.Tracker.Web/src/pages/Explore.tsx
@@ -2,6 +2,8 @@ import './Explore.css'
 import { useQuery } from "react-query"
 import { fetchEvents } from "../util/http"
 import { EventModel } from "../types/types";
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 
 type Props = {
   className: string,
@@ -10,14 +12,23 @@ type Props = {
 
 function Explore({ className, data }: Props) {
 
-    
-
     return(
-      <section className={className}>
-        {data.map(event => (
-          <p>{event.name}</p>
-        ))}
-      </section>
+      <>
+        <section className='explore__container'>
+            <MapContainer 
+            className='leaflet_map' 
+            center={[59.3369170, 18.0119609]} 
+            zoom={13}
+            scrollWheelZoom={true}>
+                <TileLayer 
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    //url="https://api.mapbox.com/styles/v1/william00771/cltvk24s1017c01pkhsjd4774/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoid2lsbGlhbTAwNzcxIiwiYSI6ImNsdHZqeWd2cTFsYzIycW9iNGlhdHFodHAifQ.eX-yYLKA0P4QCL58IgovpA"
+                />
+
+            </MapContainer>
+        </section>
+    </>
     )
 }
 
