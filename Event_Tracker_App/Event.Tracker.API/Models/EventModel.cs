1748a57a066f0061dac053f04d2f875ba0a9da26
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-10 14:23:03 +0200

    add: update -> backend EventModel, EventModelDto

--- Event_Tracker_App/Event.Tracker.API/Models/EventModel.cs
+++ Event_Tracker_App/Event.Tracker.API/Models/EventModel.cs
@@ -1,8 +1,16 @@
+using Event.Tracker.API.Models.GeocoderAPI;
+
 namespace Event.Tracker.API.Models
 {
     public class EventModel
     {
         public int Id { get; set; }
         public required string Name { get; set; }
+        public required Location Location { get; set; }
+        public required string Description { get; set; }
+        public required int Duration { get; set; }
+        public required string WebsiteUrl { get; set; }
+        public required int NumberOfPeople { get; set; }
+        public required List<string> Keywords { get; set; }
     }
 }
