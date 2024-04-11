f718670eba37d1114fdd1c1b6d58645372bd5713
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-11 08:46:46 +0200

    fix: datetime into string from data. -> Data and mockdata now interchangable

--- Event_Tracker_App/Event.Tracker.Web/src/pages/EventDetails.tsx
+++ Event_Tracker_App/Event.Tracker.Web/src/pages/EventDetails.tsx
@@ -42,7 +42,7 @@ export const EventDetails = ({ className, visible, setShowEventDetails, eventDat
                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
                                     <g id="SVGRepo_iconCarrier"> <path d="M20 10V7C20 5.89543 19.1046 5 18 5H6C4.89543 5 4 5.89543 4 7V10M20 10V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V10M20 10H4M8 3V7M16 3V7" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/> <rect x="6" y="12" width="3" height="3" rx="0.5" fill="#ffffff"/> <rect x="10.5" y="12" width="3" height="3" rx="0.5" fill="#ffffff"/> <rect x="15" y="12" width="3" height="3" rx="0.5" fill="#ffffff"/> </g>
                                 </svg>
-                                <p className='event-container__header-paragraph'>{eventData!.date.toString()}</p>
+                                <p className='event-container__header-paragraph'>{formatDateToStartDateEndDate(eventData.date, eventData.dateTo)}</p>
                             </div> 
                             <div className='event-container__header-item'>
                                 <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
@@ -50,7 +50,7 @@ export const EventDetails = ({ className, visible, setShowEventDetails, eventDat
                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
                                     <g id="SVGRepo_iconCarrier"> <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 6V12" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path d="M16.24 16.24L12 12" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </g>
                                 </svg>
-                                <p className='event-container__header-paragraph'>{eventData!.date.toString()}</p>
+                                <p className='event-container__header-paragraph'>{formatDateAndDurationToHours(eventData.time, eventData.duration)}</p>
                             </div>
                             <div className='event-container__header-item'>
                                 <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
