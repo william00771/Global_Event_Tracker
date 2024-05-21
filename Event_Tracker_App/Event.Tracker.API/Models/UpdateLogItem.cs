3935872b989782bc2cbde81212a62cd76063445b
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-05-21 15:59:16 +0200

    add: UpdateLogItem

---
+++ Event_Tracker_App/Event.Tracker.API/Models/UpdateLogItem.cs
@@ -0,0 +1,8 @@
+namespace Event.Tracker.API.Models
+{
+    public class UpdateLogItem
+    {
+        public int Id { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
--- Event_Tracker_App/Event.Tracker.API/obj/Debug/net8.0/Event.Tracker.API.AssemblyInfo.cs
+++ Event_Tracker_App/Event.Tracker.API/obj/Debug/net8.0/Event.Tracker.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41bcc2054f80189f4a5532e352d9942a580e2f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6ca80f67ccd0ed04457a579a9765662a5ac788")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
