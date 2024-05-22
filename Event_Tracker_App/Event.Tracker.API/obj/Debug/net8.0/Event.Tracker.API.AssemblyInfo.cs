14d297c20f1bb9b86678dbf6a9fd2c289d84404d
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-05-22 14:53:48 +0200

    add: UpdateLogItem -> GoogleEvsLastUpdated Prop

--- Event_Tracker_App/Event.Tracker.API/obj/Debug/net8.0/Event.Tracker.API.AssemblyInfo.cs
+++ Event_Tracker_App/Event.Tracker.API/obj/Debug/net8.0/Event.Tracker.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15719f8d7b5d3153282972eea39cb009c32a910b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d647986df6c2fc84331693b8e953f2ee93f97a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event.Tracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
